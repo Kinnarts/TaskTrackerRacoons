8a2bfc413c3967c703d153cb86509353f1d874d3
Anton Zhirkov <an.zhirkov@gmail.com>
2015-03-16 19:42:04 +0300

    project and task models

---
+++ app/models/task.rb
@@ -0,0 +1,5 @@
+class Task < ActiveRecord::Base
+  belongs_to :assigner, class_name: User
+  belongs_to :executor, class_name: User
+  belongs_to :project
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :projects, foreign_key: :owner_id
+  has_many :tasks, foreign_key: :executor_id
+  has_many :created_tasks, class_name: Task, foreign_key: :assigner_id
 end
