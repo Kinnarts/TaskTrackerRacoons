b1f5e6c067a0a7d4a3591c80d57b7fe5ba8cea0c
Anton Zhirkov <an.zhirkov@gmail.com>
2015-03-17 14:53:23 +0300

    Nested resources tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,30 @@
 class TasksController < ApplicationController
+  before_action :find_project
+
   def index
+    @tasks = @project.tasks
   end
 
-  def show
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = @project.tasks.new(task_params)
+    if @task.save
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:title, :describe, :tag_list => [])
+    params.require(:task).permit(:title, :describe, :executor_id, :assigner_id, :tag_list => [])
+  end
+
+  def find_project
+    @project = Project.find(params[:project_id])
   end
 end
