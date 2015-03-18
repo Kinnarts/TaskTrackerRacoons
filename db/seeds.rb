ee621240f5a25d9d7a7d11eb08f5cd9a5f86553a
Anton Zhirkov <an.zhirkov@gmail.com>
2015-03-18 20:30:32 +0300

    link to admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,5 @@ User.create!(email: "go@further.always", password: "freedom123", admin: false)
 User.create!(email: "admin_go@further.always", password: "freedom123", admin: true)
 
 3.times do |n|
-  Project.create!(name: Faker::Lorem.word, describe: Faker::Lorem.paragraph(2), owner_id: 2, tasks_attributes: [title: Faker::Lorem.sentence, describe: Faker::Lorem.paragraph(1), executor_id: 1, assigner_id: 2, _destroy: false, state: :created])
+  Project.create!(name: Faker::Lorem.word, describe: Faker::Lorem.paragraph(2), owner_id: 2, tasks_attributes: [title: Faker::Lorem.words(2), describe: Faker::Lorem.paragraph(1), executor_id: 1, assigner_id: 2, _destroy: false, state: :created])
 end
